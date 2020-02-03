f5f96089429690e99df0eacd63fde98129ebddd4
adrientremblay <adrien@adrientremblay.com>
2020-02-02 22:01:00 -0500

    refractored fill.ps1 completely

--- generator/fill.ps1
+++ generator/fill.ps1
@@ -1,88 +1,21 @@
-# global vars
+# Global Variables
 $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $template = "$scriptDir\master_coverletter_template.docx"
-$tempFolder = "$scriptDir\TEMP"
+$output = "$scriptDir\master_coverletter_output.docx"
 
-# unzip function
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip {
-    param([string]$zipfile, [string]$outpath)
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
+# Start Word Object
+$Word = New-Object -ComObject Word.Application
+$Word.Visible = $False
 
-# zip function
-function Zip {
-    param([string]$folderInclude, [string]$outZip)
-    [System.IO.Compression.CompressionLevel]$compression = "Optimal"
-    $ziparchive = [System.IO.Compression.ZipFile]::Open( $outZip, "Update" )
+$OpenFile = $Word.Documents.Open($template)
+$Content = $OpenFile.Content
 
-    # loop all child files
-    $realtiveTempFolder = (Resolve-Path $tempFolder -Relative).TrimStart(".\")
-    foreach ($file in (Get-ChildItem $folderInclude -Recurse)) {
-        # skip directories
-        if ($file.GetType().ToString() -ne "System.IO.DirectoryInfo") {
-            # relative path
-            $relpath = ""
-            if ($file.FullName) {
-                $relpath = (Resolve-Path $file.FullName -Relative)
-            }
-            if (!$relpath) {
-                $relpath = $file.Name
-            } else {
-                $relpath = $relpath.Replace($realtiveTempFolder, "")
-                $relpath = $relpath.TrimStart(".\").TrimStart("\\")
-            }
+# New variable for new text and variables to to replace the ones from the doc.
+$newText = $Content.Text
 
-            # add file
-            [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($ziparchive, $file.FullName, $relpath, $compression) | Out-Null
-        }
-    }
-    $ziparchive.Dispose()
-}
+$newText = $newText  -replace '<name>', 'billy'
 
-# insert function
-function Replace {
-    param([string]$keyword, [string]$value)
-    $final = "[" + $keyword + "]"
-    
-    return $body.Replace($final, $value)
-}
-
-# get info from temp.txt
-#   Index 0 -> Company Name
-#   Index 1 -> Prefix
-#   Index 2 -> First Name
-#   Index 3 -> Last Name
-#   Index 4 -> Website OR Address Line 1
-#   Index 5 -> Address Line 1 OR Address Line 2
-#   Index 6 -> City
-#   Index 7 -> Province
-#   Index 8 -> Postal Code
-$infoContents = Get-Content -Path "$scriptDir\..\scraper\temp.txt"
-
-# prepare folder
-Remove-Item $tempFolder -ErrorAction SilentlyContinue -Recurse -Confirm:$false | Out-Null
-mkdir $tempFolder | Out-Null
-
-# unzip DOCX
-Unzip $template $tempFolder
-
-# replace text
-$bodyFile = $tempFolder + "\word\document.xml"
-$body = Get-Content $bodyFile
-
-#For ($i=0; $i -le 9; $i++) {
-    
-#}
-
-$body = Replace "name" "bob marley"
-
-$body | Out-File $bodyFile -Force -Encoding ascii
-
-# zip DOCX
-$destfile = $template.Replace(".docx", "-after.docx")
-Remove-Item $destfile -Force -ErrorAction SilentlyContinue
-Zip $tempFolder $destfile
-
-# remove temp folder
-Remove-Item $tempFolder -ErrorAction SilentlyContinue -Recurse -Confirm:$false | Out-Null
+# Make the modified text the new content and Save to new document
+$Content.Text = $newText
+$OpenFile.Saveas($output)
+$OpenFile.close()
