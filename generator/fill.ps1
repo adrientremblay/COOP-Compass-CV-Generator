589c109616272fd32e74e8edfaa067c29b9625f3
adrientremblay <adrien@adrientremblay.com>
2020-02-03 00:16:18 -0500

    added signature functionality

--- generator/fill.ps1
+++ generator/fill.ps1
@@ -1,21 +1,114 @@
+# Modified Function from https://gallery.technet.microsoft.com/office/Insert-pictures-in-a-dad235ac 
+Function Add-OSCPicture {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true,Position=0)]
+        [Alias('wordpath')]
+        [String]$WordDocumentPath,
+        [Parameter(Mandatory=$true,Position=1)]
+        [Alias('imgpath')]
+        [String]$ImageFolderPath
+    )
+    
+    If(Test-Path -Path $WordDocumentPath)
+    {
+        If(Test-Path -Path $ImageFolderPath)
+        {
+            $WordExtension = (Get-Item -Path $WordDocumentPath).Extension
+            If($WordExtension -like ".doc" -or $WordExtension -like ".docx")
+            {
+                $ImageFiles = Get-ChildItem -Path $ImageFolderPath -Recurse -Include *.emf,*.wmf,*.jpg,*.jpeg,*.jfif,*.png,*.jpe,*.bmp,*.dib,*.rle,*.gif,*.emz,*.wmz,*.pcz,*.tif,*.tiff,*.eps,*.pct,*.pict,*.wpg
+                
+                If($ImageFiles)
+                {
+                    #Create the Word application object
+                    $WordAPP = New-Object -ComObject Word.Application
+                    $WordDoc = $WordAPP.Documents.Open("$WordDocumentPath")
+                
+                    Foreach($ImageFile in $ImageFiles)
+                    {
+                        $ImageFilePath = $ImageFile.FullName    
+                        
+                        $Properties = @{'ImageName' = $ImageFile.Name
+                                        'Action(Insert)' = Try
+                                                            {
+                                                                $WordAPP.Selection.EndKey(6)|Out-Null
+                                                                $WordApp.Selection.InlineShapes.AddPicture("$ImageFilePath")|Out-Null
+                                                                
+                                                                "Finished"
+                                                            }
+                                                            Catch
+                                                            {
+                                                                "Unfinished"
+                                                            }
+                                        }
+
+                        $objWord = New-Object -TypeName PSObject -Property $Properties
+                        $objWord
+                    }
+
+                    $WordDoc.Save()
+                    $WordDoc.Close()
+                    $WordAPP.Quit()#release the object
+                    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WordAPP)|Out-Null
+                    Remove-Variable WordAPP
+                }
+                Else
+                {
+                    Write-Warning "There is no image in this '$ImageFolderPath' folder."
+                }
+            }
+            Else
+            {
+                Write-Warning "There is no word document file in this '$WordDocumentPath' folder."
+            }
+        }
+        Else
+        {
+            Write-Warning "Cannot find path '$ImageFolderPath' because it does not exist."
+        }
+    }
+    Else
+    {
+        Write-Warning "Cannot find path '$WordDocumentPath' because it does not exist."
+    }
+    
+    
+}
+
 # Global Variables
 $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$template = "$scriptDir\master_coverletter_template.docx"
-$output = "$scriptDir\master_coverletter_output.docx"
+$templatePath = "$scriptDir\master_coverletter_template.docx"
+$signaturePath = "$scriptDir\signature.png"
+$outputPath = "$scriptDir\master_coverletter_output.docx"
+
+# Get info from temp.txt
+#   name->value
+$infoContents = Get-Content -Path "$scriptDir\..\scraper\temp.txt"
 
 # Start Word Object
 $Word = New-Object -ComObject Word.Application
 $Word.Visible = $False
-
-$OpenFile = $Word.Documents.Open($template)
+# Open File
+$OpenFile = $Word.Documents.Open($templatePath)
 $Content = $OpenFile.Content
 
 # New variable for new text and variables to to replace the ones from the doc.
 $newText = $Content.Text
 
-$newText = $newText  -replace '<name>', 'billy'
+# Adding employer info from temp.txt
+Foreach ($line in $infoContents) {
+    $lineSplit = $line -split "->"
+    $newText = $newText  -replace "<$($lineSplit[0])>", $lineSplit[1]
+}                         
 
 # Make the modified text the new content and Save to new document
 $Content.Text = $newText
-$OpenFile.Saveas($output)
+$OpenFile.Saveas($outputPath)
 $OpenFile.close()
+
+# Appending signature image
+Add-OSCPicture -WordDocumentPath $outputPath -ImageFolderPath $signaturePath 
+
+# Closing Message
+Write-Output 'Generation Complete!'
